866510f346cbce7fef532a54d6a17f9c32843a54
Yury Imbro <imbroyury@gmail.com>
2019-12-11 23:57:31 +0300

    requests as expansion panels

--- src/views/AllRequests.jsx
+++ src/views/AllRequests.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Paper,
+  Button,
+  Chip,
   Grid,
+  Paper,
   Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
 } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
+  paper: {
+    padding: '20px',
+  },
+  method: {
+    marginRight: '20px',
+  }
 });
 
 function AllRequests() {
@@ -21,23 +33,47 @@ function AllRequests() {
       .then(response => response.json())
       .then(requests => setRequests(requests))
       .catch(e => setError(e));
-  }, []) // componentDidMount - empty dependecy array
+  }, []) // componentDidMount - empty dependency array
+
+  const renderError = () => (<Paper className={classes.paper}>
+    <Typography variant="h4">Error while fetching requests</Typography>
+  </Paper>)
+
+  const renderNoRequests = () => (<Paper className={classes.paper}>
+    <Typography variant="h4">No requests fetched</Typography>
+  </Paper>)
 
   return (<Grid container>
-    {error && <Typography variant="h4">Error while fetching requests</Typography>}
-    {requests.length === 0 && <Typography variant="h4">No requests currently created</Typography>}
-      {
-        requests.map(request =>
-        <Paper className={classes.paper} key={request._id}>
-          <Grid container>
-            {
-              Object
-                .entries(request)
-                .map(([k, v]) => <Grid item key={k}>{k} - {JSON.stringify(v)}</Grid>)
-            }
-          </Grid>
-        </Paper>)
-      }
+    {error && renderError()}
+    {requests.length === 0 && renderNoRequests()}
+    {
+      requests.map(request =>
+        (<ExpansionPanel key={request._id}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Grid container alignItems="center">
+              <Chip label={request.requestMethod} color="primary" className={classes.method} />
+              <Typography variant="subtitle2"> {request.requestUrl}</Typography>
+            </Grid>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Grid container>
+              {
+                Object
+                  .entries(request)
+                  .map(([k, v]) => (<Grid item container key={k}>
+                    <Typography>{k} - {JSON.stringify(v)}</Typography>
+                  </Grid>))
+              }
+              <Button variant="outlined" color="primary">Run</Button>
+            </Grid>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>)
+      )
+    }
   </Grid>);
 }
 
